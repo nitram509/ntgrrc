58b26d591ef950ee89370a87589d348cae48f73a
nitram509 <maki@bitkings.de>
2022-04-21 10:10:13 +0200

    implement password encryption

--- login.go
+++ login.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
+	"math"
+	"strings"
 )
 
-func getRandomSeedValueFromLoginHtml(reader io.Reader) (string, error) {
+func getSeedValueFromLoginHtml(reader io.Reader) (string, error) {
 	doc, err := goquery.NewDocumentFromReader(reader)
 	if err != nil {
 		return "", err
@@ -18,4 +22,26 @@ func getRandomSeedValueFromLoginHtml(reader io.Reader) (string, error) {
 	}
 	return "", errors.New("random seed value not found in login.cgi response. " +
 		"An element with id=rand and an attribute 'value' is expected")
+}
+
+// encryptPassword re-implements some logic from Netgear's GS305EP frontend component, see login.js
+func encryptPassword(password string, seedValue string) string {
+	mergedStr := specialMerge(password, seedValue)
+	hash := md5.New()
+	io.WriteString(hash, mergedStr)
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func specialMerge(password string, seedValue string) string {
+	result := strings.Builder{}
+	maxLen := int(math.Max(float64(len(password)), float64(len(seedValue))))
+	for i := 0; i < maxLen; i++ {
+		if i < len(password) {
+			result.WriteString(string([]rune(password)[i]))
+		}
+		if i < len(seedValue) {
+			result.WriteString(string([]rune(seedValue)[i]))
+		}
+	}
+	return result.String()
 }
--- login__test.go
+++ login__test.go
@@ -8,11 +8,17 @@ import (
 	"testing"
 )
 
-func TestGetRandomSeedValueFromLoginHtml(t *testing.T) {
-	randomVal, err := getRandomSeedValueFromLoginHtml(strings.NewReader(loginCgiHtml))
+func TestGetSeedValueFromLoginHtml(t *testing.T) {
+	randomVal, err := getSeedValueFromLoginHtml(strings.NewReader(loginCgiHtml))
 
 	then.AssertThat(t, randomVal, is.EqualTo("1761741982"))
 	then.AssertThat(t, err, is.Nil())
+}
+
+func TestEncryptPassword(t *testing.T) {
+	val := encryptPassword("foobar", "12345678")
+
+	then.AssertThat(t, val, is.EqualTo("d1f4394e3e212ab4f06e08c54477a237"))
 }
 
 //go:embed test-data/login.cgi.html
