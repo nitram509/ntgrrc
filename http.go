848e85005de56480736ab4404ddf9668a5f47655
davidk <kdavid@gmail.com>
2022-08-27 05:44:59 -0700

    http: Consolidate GET and POST
    Co-authored-by: nitram509 <maki@bitkings.de>

--- http.go
+++ http.go
@@ -7,37 +7,14 @@ import (
 )
 
 func requestPage(args *GlobalOptions, host string, url string) (string, error) {
-	token, err := loadToken(args, host)
-	if err != nil {
-		return "", err
-	}
-
-	if args.Verbose {
-		println("Fetching data from: " + url)
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, strings.NewReader(""))
-	if err != nil {
-		return "", err
-	}
-
-	req.Header.Set("Cookie", "SID="+token)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	if args.Verbose {
-		println(resp.Status)
-	}
-	bytes, err := io.ReadAll(resp.Body)
-	return string(bytes), err
+	return doHttpRequestAndReadResponse(args, http.MethodGet, host, url, "")
 }
 
-func postPage(args *GlobalOptions, host string, postUrl string, data string) (string, error) {
+func postPage(args *GlobalOptions, host string, url string, requestBody string) (string, error) {
+	return doHttpRequestAndReadResponse(args, http.MethodPost, host, url, requestBody)
+}
 
+func doHttpRequestAndReadResponse(args *GlobalOptions, httpMethod string, host string, postUrl string, requestBody string) (string, error) {
 	token, err := loadToken(args, host)
 	if err != nil {
 		return "", err
@@ -47,7 +24,7 @@ func postPage(args *GlobalOptions, host string, postUrl string, data string) (st
 		println("Fetching data from: " + postUrl)
 	}
 
-	req, err := http.NewRequest(http.MethodPost, postUrl, strings.NewReader(data))
+	req, err := http.NewRequest(httpMethod, postUrl, strings.NewReader(requestBody))
 	if err != nil {
 		return "", err
 	}
